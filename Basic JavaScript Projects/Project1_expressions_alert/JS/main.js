755f69f32ba6fb5df22d23ad25d2817cdd7a4691
Shay R <76569624+shay-d16@users.noreply.github.com>
2021-02-15 23:47:51 -0800

    Alert and Text
    Link my main.js file to my index(1).html file. Use the alert attribute and the document.write attribute in JavaScript.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var X = "Hello, World!"
+
+window.alert(X);
+
+var S = " My name is Shalondra Rossiter, but you can call me Shay. <br>"
+
+document.write(S)
+
+var I = " \"You must find the place inside yourself where nothing is impossible.\" <br> -Deepak Chopra"
+
+document.write(I)
