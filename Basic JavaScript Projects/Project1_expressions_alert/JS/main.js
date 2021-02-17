aa99194162d7c04a1d54f86062ab045e9efcba07
Shay R <76569624+shay-d16@users.noreply.github.com>
2021-02-16 19:36:32 -0800

    Variables
    Assign multiple variables in one statement. Experiment with font colors.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,10 +2,30 @@ var X = "Hello, World!"
 
 window.alert(X);
 
-var S = " My name is Shalondra Rossiter, but you can call me Shay. <br>"
+var S = " My name is Shalondra Rossiter, but you can call me Shay. <br><br>"
 
 document.write(S)
 
-var I = " \"You must find the place inside yourself where nothing is impossible.\" <br> -Deepak Chopra"
+var I = " \"You must find the place inside yourself where nothing is impossible.\" <br> -Deepak Chopra <br><br>"
 
 document.write(I)
+
+var B = " \"Within every woman there is a wild and natural creature,"
++ " a powerful force, filled with good instincts,"
++ " passionate creativity, and ageless knowing.\""
++ " -Clarissa Pinkola Estes <br><br>"
+
+document.write(B)
+
+var fourAgreements = "\"The Four Agreements\" <br>", One = " -Be Impeccable With Your Word <br>", Two = " -Don\'t Take Anything Personally <br>", Three = " -Don\'t Make Assumptions <br>", Four = " -Always Do Your Best <br>", fourAgreementsAuthor = "<br> Author: don Miguel Ruiz <br>";
+var One = One.fontcolor("palevioletred");
+var Two = Two.fontcolor("lightcoral");
+var Three = Three.fontcolor("cornflowerblue");
+var Four = Four.fontcolor("plum");
+
+document.write(fourAgreements)
+document.write(One)
+document.write(Two)
+document.write(Three)
+document.write(Four)
+document.write(fourAgreementsAuthor)
