46764f0c6379e38e9bedfa13e9a06024ba9aae24
Shay R <76569624+shay-d16@users.noreply.github.com>
2021-02-22 17:05:27 -0800

    JavaScript Functions
    Utilize events and functions in JavaScript on an HTML file.

---
+++ Basic JavaScript Projects/Project2_funtions/JS/main.js
@@ -0,0 +1,5 @@
+function shaysFunction() {
+    var sentence = "This is what I've been learning";
+    sentence += " in JavaScript so far!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
