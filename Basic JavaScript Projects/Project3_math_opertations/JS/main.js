11dcbb93fc0caa74ffc7c57035d648ab4733095a
Shay R <shalondra.davis@protonmail.com>
2021-02-28 11:19:16 -0800

    Addition and Division Funcions
    Create functions that multiply or divide a set of numbers and returns the value of each operation.

--- Basic JavaScript Projects/Project3_math_opertations/JS/main.js
+++ Basic JavaScript Projects/Project3_math_opertations/JS/main.js
@@ -7,3 +7,13 @@ function subtraction_Function() { //Create a function that subtractions two numb
     var subtraction = 5 - 2; //Set the return value of 5 - 2 as the subtrattion variable
     document.getElementById("Subtracting").innerHTML = "5 - 2 = " + subtraction; //Call Subtraction return value to the HTML file
 }
+
+function multiplication() { //Create a function that multiplies two numbers
+    var simple_Math = 6 * 8; //Set the return value of the operation
+    document.getElementById("Multiplying").innerHTML = "6 x 8 = " + simple_Math; //Call the function and its return value to the HTML file
+}
+
+function division() { //Create a funciton that divides two numbers
+    var simple_Math = 48 / 6; //Set the return value
+    document.getElementById("Dividing").innerHTML = "48 / 6 = " + simple_Math;
+} 
