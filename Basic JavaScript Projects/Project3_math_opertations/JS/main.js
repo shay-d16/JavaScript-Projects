e50f5c98ce9eb8b228336a40f584cb0ea07fac1a
Shay R <shalondra.davis@protonmail.com>
2021-02-27 23:18:45 -0800

    Addition and Subtraction Functions
    Create addition and subtraction functions that each return its respective value. Utilize the button element and give each button an event handler.

---
+++ Basic JavaScript Projects/Project3_math_opertations/JS/main.js
@@ -0,0 +1,9 @@
+function addition_Function() { //Create a new function called addition_Function
+    var addition = 2 + 2; //Set addition variable to 2 + 2
+    document.getElementById("Adding").innerHTML = "2 + 2 = " + addition; // Call the Addition return value to the HTML file
+}
+
+function subtraction_Function() { //Create a function that subtractions two numbers and returns the value
+    var subtraction = 5 - 2; //Set the return value of 5 - 2 as the subtrattion variable
+    document.getElementById("Subtracting").innerHTML = "5 - 2 = " + subtraction; //Call Subtraction return value to the HTML file
+}
