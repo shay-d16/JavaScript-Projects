dec17d7647d812ecc57ed2cf072a775c05fba20d
Shay R <shalondra.davis@protonmail.com>
2021-03-01 17:09:01 -0800

    Delete Operator
    Use delete operator to delete a key before it is displayed.

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,11 @@
-function my_Dictionary() {
-    var Tarot = {
+function my_Dictionary() { //Create a Dictionary function
+    var Tarot = { //Set the variable to Tarot
         Card: " The High Priestess",
         Category: " Major Arcana",
         Element: " Water",
         Up_Key: " Upright Keywords: Intuition, sacred knowledge, divine feminine, the subconscious mind",
         Rev_Key: " Reversed Keywords: Secrets disconnected from intuition, withdrawl and silence",
     };
-    document.getElementById("Dictionary").innerHTML = Tarot.Card + "<br>" + Tarot.Category;
-}
+    delete Tarot.Element; //Delete this key before the value is displayed
+    document.getElementById("Dictionary").innerHTML = Tarot.Card + "<br>" + Tarot.Category + "<br>" + Tarot.Element;
+} //Call the function and display each value, Tarot.Element should instead return 'undefined' on the .html file
