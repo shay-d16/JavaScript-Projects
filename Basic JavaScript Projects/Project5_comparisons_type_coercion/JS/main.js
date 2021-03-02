bfcce39af88bd3544550e2d87e4aeaa3423d2b70
Shay R <shalondra.davis@protonmail.com>
2021-03-02 13:24:55 -0800

    NaN and isNaN
    Create a NaN function, as well as two isNaN functions.

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,13 @@
 document.write(typeof "heartbeat");
+
+function my_Function() {
+document.getElementById("Test").innerHTML = 0/0;
+}
+
+function isNaN_function1() {
+    document.getElementById("isNaN Test1").innerHTML = isNaN("12467");
+}
+
+function isNaN_function2() {
+    document.getElementById("isNaN Test2").innerHTML = isNaN("Shay was here!");
+}
