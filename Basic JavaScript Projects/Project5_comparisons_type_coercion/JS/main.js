579b5ed2fe32d97ae04d7ef4ea0a7d927705823d
Shay R <shalondra.davis@protonmail.com>
2021-03-03 14:32:02 -0800

    Double and Triple Equal Signs
    Utilize double and triple equal signs in to create true/false statements.

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,29 +1,68 @@
-document.write(typeof "heartbeat"); //Display what data type 'heartbeat' is
+//BOOLEAN LOGIC
+document.write("<h3><u>True or False</u></h3>"); //Underlined heading
 
+document.write("65 is greater than 68: ");
+document.write(65 > 68); //Use Boolean logic to determine if the condition is true or false: 'false'
 document.write("<br><br><br>"); //Line breaks
 
-document.write(65 > 68); //Use Boolean logic to determine if the condition is true or false
-
+document.write("34 is less than 45: ");
+document.write(34 < 45); //Use Boolen logic to determine if the condition is true or false: 'true'
 document.write("<br><br><br>"); //Line breaks
 
-document.write(34 < 45); //Use Boolen logic to determine if the condition is true or false
-
-document.write("<br><br><br>"); //Line breaks
-
-document.write("12" + 4); //Utilize type coercion
-
-document.write("<br><br><br>"); //Line breaks
-
+//DOUBLE EQUAL SIGNS
+document.write("74 is equal to 74: ");
 document.write(74 == 74); //Utilize == to return "true"
-
 document.write("<br><br><br>"); //Line breaks
 
+document.write("58 is equal to 65: ");
 document.write(58 == 65); //Utilize == to return "false"
+document.write("<br><br><br>"); //Line breaks
 
+//TRIPLE EQUAL SIGNS
+var x = 115; //Assign variable 'x' as a number data type
+var y = 115; //Assign variable 'y' as a number data type
+document.write("115 and 115 have the same data type and value: ");
+document.write(x === y); //Return 'true' by ensuring to match the data type AND value
+document.write("<br><br><br>"); //Line breaks
+
+var q = "Twenty"; //Assign variable 'q' as a string data type
+var r = 21; //Assign variable 'r' as a number data type
+document.write("\"Twenty\" and 21 have the same data type and value: ");
+document.write(q === r); //Return 'false' by writing a different data type and different value
+document.write("<br><br><br>"); //Line breaks
+
+var a = "32"; //Assign variable 'a' as a string data type
+var b = 32; //Assign variable 'b' as a number data type
+document.write("\"32\" and 32 have the same data type and value: ");
+document.write(a === b); //Return 'false' by displaying a different data type but the same value for both
+document.write("<br><br><br>"); //Line breaks
+
+var f = "Duck"; //Assign variable 'f' as a string data type
+var g = "Ducks"; //Assign variable 'g' as a string data type
+document.write("\"Duck\" and \"Ducks\" have the same data type and value: ");
+document.write(f === g); //Return 'false' by writing the same data type but a different value for both
+
+//TYPEOF FUNCTION
+document.write("<h3><u>Typeof Operator</u></h3>"); //Underlined heading
+
+document.write("What data type is \"heartbeat\": ");
+document.write(typeof "heartbeat"); //Display what data type 'heartbeat' is
+document.write("<br><br><br>"); //Line breaks
+
+
+//TYPE COERCION
+document.write("<h3><u>Type Coercion</u></h3>"); //Underlined heading
+
+document.write("\"12\" + 4 = ");
+document.write("12" + 4); //Utilize type coercion
+document.write("<br><br><br>"); //Line breaks
+
+//NOT A NUMBER FUNCTION
 function my_Function() { //Test the NaN function to determine if the return value of 0/0 is a number or not
 document.getElementById("Test").innerHTML = 0/0; //Diplay return value in the .html file
 } //Return value: NaN (Not a Number)
 
+//IS NOT A NUMBER FUNCTION
 function isNaN_function1() { //Create a function to ask the computer if it is true or false that '12467' is NOT a number
     document.getElementById("isNaN Test1").innerHTML = isNaN("12467"); //Display return value to the .html file
 } //Return value: false
@@ -32,7 +71,10 @@ function isNaN_function2() { //Create a function to ask the computer if it is tr
     document.getElementById("isNaN Test2").innerHTML = isNaN("Shay was here!"); //Diplay return value in the .html file
 } //Return value: true
 
+//CONSOLE LOG FUNCTION
 console.log(12 * 10); //Perform a math operation with the console.log() function
 
 console.log(101 > 103); //Use Boolean Logic and console.log() to display "false" in the console
+
+
 
