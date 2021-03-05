a401449ee4a021aa08df661cbda71515baab8d5e
Shay R <shalondra.davis@protonmail.com>
2021-03-04 19:23:04 -0800

    Constructors and Nested Functions
    Utilize constructors, "new" and "this" keywords, and  nested functions in JavaScript.

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/key.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/key.js
@@ -11,3 +11,23 @@ function myFunction() {
  document.getElementById("Keywords_and_Constructors").innerHTML = 
  "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufatured in " + Erik.Vehicle_Year
 }
+
+//Constructor function for the Film object
+function Film(Title, Year, Director, Runtime) {
+    this.Film_Title = Title;
+    this.Film_Year = Year;
+    this.Film_Director = Director;
+    this.Film_Runtime = Runtime;
+}
+var Mummy = new Film("<i>The Mummy</i>", 1999, "Steven Sommers", "2h 4m");
+var Thor = new Film("<i>Thor: Ragnarok</i>", 2017, "Taika Waititi", "2h 10m");
+var Blonde = new Film("<i>Legally Blonde</i>", 2001, "Robert Luketic", "1h 36m");
+var Witch = new Film("<i>The Love Witch</i>", 2016, "Anna Biller", "2h");
+var Midsommer = new Film("<i>Midsommer</i>", 2019, "Ari Aster", "2h 28m");
+var Practical = new Film("<i>Practical Magic</i>", 1998, "Griffin Dunne", "1h 44m");
+function favFilms() {
+    document.getElementById("Film_Constructor").innerHTML =
+    "One of my favorite films is " + Mummy.Film_Title +
+    ", made in " + Mummy.Film_Year + " and directed by " + 
+    Mummy.Film_Director + "."
+}
--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,6 +1,19 @@
+//TERNARY OPERATOR
 function age_Function() { //Create a function in which you can utilize ternary operators to make a program that outputs whether or not the user is old enough to vote
     var Age, Can_vote; //Assign two variables
     Age = document.getElementById("Age").value; //
     Can_vote = (Age < 18) ? "You are not old enough to vote":"You can vote!"; //Using the ternary operator '?' set the condition for the value of the variable
     document.getElementById("Voting Age").innerHTML = Can_vote; //
+}
+
+
+//NESTED FUNCTION
+function nestedFunction() { //Create a function containing a nested function
+    document.getElementById("Nested_Function").innerHTML = add(); //Call the add() function
+    function add() { //Create nested add() function
+        var counter = 0; //Set the counter to 0
+        function plus() {counter += 1;} //Create another nested function that will add '1' to the counter
+        plus();
+        return counter; //Display return value
+    }
 }
