b3c1ab312a40340cd2dabf6de6fba8ffe0934050
Shay R <shalondra.davis@protonmail.com>
2021-03-08 17:14:57 -0800

    Loops
    Utilize both While and For Loops in JavaScript

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,21 @@
+//WHILE LOOP
+function counting_To_Ten() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++; //Use the increment operator (++) to add on in JavaScript
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
+
+//FOR LOOP
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
--- Basic JavaScript Projects/Project10_loops_arrays/JS/while_loop.js
+++
@@ -1,9 +0,0 @@
-function count_To_Ten() {
-    var Digit = "";
-    var X = 1;
-    while (X < 11) {
-        Digit += "<br>" + X;
-        X++; //Use the increment operator (++) to add on in JavaScript
-    }
-    document.getElementById("Counting_to_Ten").innerHTML = Digit;
-}
