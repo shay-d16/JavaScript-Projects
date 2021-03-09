45adb5b4408bebb746998909c33d901e0f0e3898
Shay R <shalondra.davis@protonmail.com>
2021-03-08 17:49:43 -0800

    Arrays
    Utilize arrays in JavaScript and concatenate them.

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -23,6 +23,31 @@ function for_Loop() {
 //STRING LENGTH
 function string_Length_Function() {
     var str = "If you\'re a bird, I\'m a bird.";
-    var nb = str.length;
+    var nb = str.length; //The length property returns the length of a string (number of characters)
     document.getElementById("String_length").innerHTML = nb;
+}
+
+//ARRAYS
+function cat_pics() {
+    var Cat_Picture = []; //'Cat_Picture' is the object
+    Cat_Picture[0] = "sleeping"; 
+    Cat_Picture[1] = "playing"; //0, 1, 2, and 3 are the indexes
+    Cat_Picture[2] = "eating"; 
+    Cat_Picture[3] = "purring";
+    //'sleeping', 'playing', 'eating', and 'purring' are the properties of the object
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
+        Cat_Picture[2] + ".";
+}
+
+function array_Function() {
+    var Emotions = [];
+    Emotions[0] = "energetic";
+    Emotions[1] = "joyful";
+    Emotions[2] = "exhausted";
+    Emotions[3] = "disappointed";
+    Emotions[4] = "melancholy";
+    Emotions[5] = "inspired";
+    Emotions[6] = "motivated";
+    document.getElementById("Array").innerHTML = "Today I am feeling " + Emotions[0] +
+        ", " + Emotions[6] + ", and " + Emotions[5] + "!";
 }
